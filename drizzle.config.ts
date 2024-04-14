8a493f83907663cdb27fcd2281244cae793de227
kdh379 <ehgus37625@naver.com>
2024-04-14 21:39:33 +0900

    feat: drizzle

---
+++ drizzle.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "drizzle-kit";
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString)
+  throw new Error("Please define the DATABASE_URL environment variable inside .env");
+
+export default {
+  schema: "./src/schema.ts",
+  out: "./drizzle",
+  driver: "pg",
+  dbCredentials: {
+    connectionString,
+  },
+} satisfies Config;
---
+++ src/core/db.ts
@@ -0,0 +1,13 @@
+import "dotenv/config";
+
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString)
+  throw new Error("Please define the DATABASE_URL environment variable inside .env");
+
+// Disable prefetch as it is not supported for "Transaction" pool mode
+export const client = postgres(connectionString, { prepare: false });
+export const db = drizzle(client);
