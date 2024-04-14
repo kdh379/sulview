1ef5f8b745af6e07f6058cf156a1d8a38b5e59df
kdh379 <ehgus37625@naver.com>
2024-04-14 21:38:46 +0900

    feat: init

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+  name: "sulview",
+  description: "위스키 리뷰 사이트",
+  image: "/images/profile.png",
+  links: {
+    github: "https://github.com/kdh379/sulview",
+  },
+  keywords: [
+    "위스키",
+    "리뷰",
+  ],
+};
