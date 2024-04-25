5a43ff45fa472b47311feb4856deeff1b719d2d4
kdh379 <ehgus37625@naver.com>
2024-04-25 18:32:47 +0900

    chore: delete backup files

--- _back/drizzle/0003_shiny_korath.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "distillery" ALTER COLUMN "id" SET DATA TYPE serial;--> statement-breakpoint
-ALTER TABLE "whisky" ALTER COLUMN "abv" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "whisky" ALTER COLUMN "age" SET DATA TYPE text;--> statement-breakpoint
-ALTER TABLE "whisky" ALTER COLUMN "caskType" SET DATA TYPE text[];--> statement-breakpoint
-ALTER TABLE "review" ADD COLUMN "id" serial NOT NULL;--> statement-breakpoint
-ALTER TABLE "review" ADD COLUMN "images" text[] NOT NULL;--> statement-breakpoint
-ALTER TABLE "distillery" DROP COLUMN IF EXISTS "country";
