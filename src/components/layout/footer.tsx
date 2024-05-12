33a782fafd9ed1f500fbd34e004898ba8c28d5e8
kdh379 <ehgus37625@naver.com>
2024-05-13 03:18:32 +0900

    chore(footer): 스타일 변경

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -7,29 +7,31 @@ import { siteConfig } from "@/config/site";
 
 export default function Footer() {
   return (
-    <footer className="container flex h-14 items-center justify-end gap-x-4 font-bold">
-      <Link
-        href={siteConfig.links.github}
-        target="_blank"
-        rel="noopener noreferrer"
-        className={buttonVariants({
-          size: "icon",
-          variant: "outline",
-        })}
-      >
-        <GitHubLogoIcon />
-        <span className="sr-only">Github</span>
-      </Link>
-      <Link
-        href={siteConfig.links.mailTo}
-        className={buttonVariants({
-          size: "icon",
-          variant: "outline",
-        })}
-      >
-        <Mail className="size-4" />
-        <span className="sr-only">Mail</span>
-      </Link>
+    <footer className="container mt-auto">
+      <nav className="mb-4 flex gap-x-4">
+        <Link
+          href={siteConfig.links.github}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={buttonVariants({
+            size: "icon",
+            variant: "outline",
+          })}
+        >
+          <GitHubLogoIcon />
+          <span className="sr-only">Github</span>
+        </Link>
+        <Link
+          href={siteConfig.links.mailTo}
+          className={buttonVariants({
+            size: "icon",
+            variant: "outline",
+          })}
+        >
+          <Mail className="size-4" />
+          <span className="sr-only">Mail</span>
+        </Link>
+      </nav>
       <p>© 2024 {siteConfig.name}</p>
     </footer>
   );
