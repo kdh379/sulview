707e0612559f030e14b5840ff101dcfe04ac25c3
kdh379 <ehgus37625@naver.com>
2024-06-11 20:48:19 +0900

    feat: main page
    - hero 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
+import Hero from "@/app/hero";
+
 export default async function HomePage() {
   return (
-    <div className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
-      home
-      <p>aaaaaa</p>
-    </div>
+    <Hero />
   );
 }
