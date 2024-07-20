7c4c04683914198c3f0e5832a5659542a37ddfb9
kdh379 <ehgus37625@naver.com>
2024-07-20 16:32:41 +0900

    feat: progress animation

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -4,22 +4,38 @@ import * as ProgressPrimitive from "@radix-ui/react-progress";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
+import { useIntersectionObserver } from "@/hooks/useIntersectionObserver";
+
+const DEFAULT_DURATION = 500;
 
 const Progress = React.forwardRef<
   React.ElementRef<typeof ProgressPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & { value: number }
->(({ className, value, ...props }, ref) => (
-  <ProgressPrimitive.Root
-    ref={ref}
-    className={cn("bg-primary/20 relative h-2 w-full overflow-hidden rounded-full", className)}
-    {...props}
-  >
-    <ProgressPrimitive.Indicator
-      className="bg-primary size-full flex-1 transition-all"
-      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
-    />
-  </ProgressPrimitive.Root>
-));
+  React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> & { value: number, duration?: number }
+>(({ className, value, duration = DEFAULT_DURATION, ...props }, ref) => {
+  const { isIntersecting, targetRef } = useIntersectionObserver();
+  const [progress, setProgress] = React.useState(0);
+
+  React.useEffect(() => {
+    if (!isIntersecting) return;
+
+    const timer = setTimeout(() => setProgress(value), duration);
+    return () => clearTimeout(timer);
+  }, [isIntersecting, value, duration]);
+
+  return (
+    <ProgressPrimitive.Root
+      ref={ref}
+      className={cn("bg-primary/20 relative h-2 w-full overflow-hidden rounded-full", className)}
+      {...props}
+    >
+      <ProgressPrimitive.Indicator
+        ref={targetRef}
+        className="bg-primary size-full flex-1 transition-all"
+        style={{ transform: `translateX(-${100 - (progress || 0)}%)` }}
+      />
+    </ProgressPrimitive.Root>
+  );
+});
 Progress.displayName = ProgressPrimitive.Root.displayName;
 
 export { Progress };
